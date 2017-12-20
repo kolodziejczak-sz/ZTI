c25a55d6873a383df3d62f8b43d36e37ed9a0976
Tomasz <kottomasz123@gmail.com>
2017-12-20 11:55:03 +0100

    switch to turtle

--- backend/server/controllers/rdfController.js
+++ backend/server/controllers/rdfController.js
@@ -1,117 +1,56 @@
 module.exports = {
- queryRdfStore : function queryRdfStore(query, callback) {
-  "use strict";
-  var rdfstore = require('rdfstore')
-  rdfstore.create(function (err, store) {
+  queryRdfStore: function queryRdfStore(query, callback) {
+    "use strict";
+    var fs = require('fs');
+    var rdfstore = require('rdfstore')
+    var path = require('path');
+    rdfstore.create(function (err, store) {
 
-    var sample_owl_json = JSON.parse("[ {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Ontology\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#ABC_z.o.o.\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#NamedIndividual\", \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#firma\" ],\n" +
-      "  \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#znajduje_sie_w\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Poznan\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#HR\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ],\n" +
-      "  \"http://www.w3.org/2000/01/rdf-schema#subClassOf\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#dzial\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#IT\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ],\n" +
-      "  \"http://www.w3.org/2000/01/rdf-schema#subClassOf\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#dzial\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Jacek_Nowak\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#NamedIndividual\" ],\n" +
-      "  \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#jest_podwladnym\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Jan_Kowalski\"\n" +
-      "  } ],\n" +
-      "  \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#pracuje_w\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#ABC_z.o.o.\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Jan_Kowalski\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#NamedIndividual\", \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#kierownik\" ],\n" +
-      "  \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#jest_przelozonym\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Jacek_Nowak\"\n" +
-      "  }, {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Robert_Kwasniewski\"\n" +
-      "  } ],\n" +
-      "  \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#pracuje_w\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#ABC_z.o.o.\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Poznan\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#NamedIndividual\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Robert_Kwasniewski\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#NamedIndividual\" ],\n" +
-      "  \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#jest_podwladnym\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#Jan_Kowalski\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#dzial\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#firma\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#jest_podwladnym\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#ObjectProperty\" ],\n" +
-      "  \"http://www.w3.org/2002/07/owl#inverseOf\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#jest_przelozonym\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#jest_przelozonym\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#ObjectProperty\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#kierownik\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ],\n" +
-      "  \"http://www.w3.org/2000/01/rdf-schema#subClassOf\" : [ {\n" +
-      "    \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#pracownik\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#miasto\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#pracownik\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#pracuje_w\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#ObjectProperty\" ],\n" +
-      "  \"http://www.w3.org/2000/01/rdf-schema#subPropertyOf\" : [ {\n" +
-      "    \"@id\" : \"http://www.w3.org/2002/07/owl#topObjectProperty\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#sektor\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#Class\" ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#zarzadza\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#ObjectProperty\" ],\n" +
-      "  \"http://www.w3.org/2000/01/rdf-schema#subPropertyOf\" : [ {\n" +
-      "    \"@id\" : \"http://www.w3.org/2002/07/owl#topObjectProperty\"\n" +
-      "  } ]\n" +
-      "}, {\n" +
-      "  \"@id\" : \"http://www.semanticweb.org/ja/ontologies/2017/2/untitled-ontology-7#znajduje_sie_w\",\n" +
-      "  \"@type\" : [ \"http://www.w3.org/2002/07/owl#ObjectProperty\" ]\n" +
-      "} ]");
+      console.log(__dirname)
+      var filePath = path.join(__dirname, '..', '..', '..', 'ontology', 'phones-turtle.owl');
+      var inputString = fs.readFileSync(filePath).toString()
 
-    store.load("application/ld+json", sample_owl_json, function (err, results) {
+      const type = "text/turtle"//"application/ld+json"
+      store.load(type, inputString, function (err, results) {
+        console.log("elements loaded into store: " + results);
 
-      console.log("elements loaded into store: " + results);
+        store.execute(query, function (err, results) {
 
-      store.execute(query, function (err, results) {
-
-        console.log(results);
-        callback(results)
+          console.log(results);
+          callback(results)
+        });
       });
     });
-  });
-}
+
+    console.log(TEST)
+
+  },
+
+  loadStore: function loadStore() {
+
+    var fs = require('fs');
+    var rdfstore = require('rdfstore')
+    var path = require('path');
+
+    const filePath = path.join(__dirname, '..', '..', '..', 'ontology', 'phones-turtle.owl');
+    var inputString = fs.readFileSync(filePath).toString()
+    const type = "text/turtle"//"application/ld+json"
+    var full_store
+    var s = rdfstore.create(function (err, store) {
+
+      console.log(__dirname)
+
+      store.load(type, inputString, function (err, results) {
+        console.log("elements loaded into store: " + results);
+        full_store = store
+      })
+
+    })
+    while (full_store === undefined) {
+      require('deasync').runLoopOnce()
+    }
+    return full_store
+  }
+
 }
 
--- backend/server/server.js
+++ backend/server/server.js
@@ -6,14 +6,118 @@ var rdfController = require("../../backend/server/controllers/rdfController.js")
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
-
+var rdf_s = rdfController.loadStore()
 app.listen(port);
 
 app.put('/', function (req, res) {
   console.log(req.body)
-  rdfController.queryRdfStore(req.body.query, function(result) {
+  var params = req.query
+  var sparql_query = req.body.query
+
+  rdf_s.execute(sparql_query, function (err, result) {
+    console.log(result)
     res.send(result)
   })
 });
 
-console.log('REST API started on: ' + port);
+//example: AmoiM360
+app.get('/api/detail/:model', function (req, res) {
+  console.log('looking for data about: '+req.params.model)
+  var sparql_query = "SELECT * {<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#"+req.params.model+"> ?p ?o }"
+  rdf_s.execute(sparql_query, function (err, result) {
+    console.log(result)
+    res.send(result)
+  })
+})
+
+app.get('/api/list/os', function (req, res) {
+  console.log('listing operating systems')
+  var sparql_query = "" +
+    "@PREFIX phones: <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>" +
+    "Select * WHERE {?s <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones:has_os> ?o}"
+  rdf_s.execute(sparql_query, function (err, result) {
+    console.log(result)
+    res.send(result)
+  })
+})
+
+app.get('/api/list;matrix-uri', function (req, res) {
+  var criteria = parse(req.params.matrix-uri)
+  console.log('looking for phones fulfilling given criteria: '+criteria)
+
+  if ('ram_from' in criteria){
+    var ram_from = criteria.ram_from
+  }
+  if ('ram_to' in criteria){
+    var ram_to = criteria.ram_to
+  }
+  if ('os' in criteria){
+    var os = criteria.os
+  }
+  if ('price_from' in criteria){
+    var price_from = criteria.price_from
+  }
+  if ('price_to' in criteria){
+    var price_to = criteria.price_to
+  }
+  if ('display_inch_from' in criteria){
+    var display_inch_from = criteria.display_inch_from
+  }
+  if ('display_inch_to' in criteria){
+    var display_inch_to = criteria.display_inch_to
+  }
+  if ('brand' in criteria){
+    var brand = criteria.brand
+  }
+
+  filters = []
+  filters.append(ram_from === undefined ? "":"xsd:integer(?o) > "+ram_from)
+  filters.append(ram_to === undefined ? "":"xsd:integer(?o) < "+ram_to)
+  filters.append(os === undefined ? "":"regex(?p,'"+os+"','i')")
+  filters.append(brand === undefined ? "":"regex(?p,'"+brand+"','i')")
+  filters.append(price_from === undefined ? "":"xsd:integer(?o) > "+price_from)
+  filters.append(price_to === undefined ? "":"xsd:integer(?o) < "+price_to)
+  filters.append(display_inch_from === undefined ? "":"xsd:integer(?o) > "+display_inch_from)
+  filters.append(display_inch_to === undefined ? "":"xsd:integer(?o) < "+display_inch_to)
+  filters = filtes.filter(function (element) {
+    return element !== ""
+  })
+
+  var filter_string = filters.join(" && ")
+
+  var sparql_query = "" +
+    "SELECT ?s " +
+    "WHERE "+
+    "{?s a :phone" +
+    " ?p" +
+    " ?o }" +
+    "FILTER(" +
+    "" +
+     filter_string
+      +
+    ")"
+  rdf_s.execute(sparql_query, function (err, result) {
+    console.log(result)
+    res.send(result)
+  })
+})
+
+
+console.log('REST API started on: http://localhost:' + port);
+
+function parse(input) {
+  const splitCharMain = ";"
+  const splitCharSecondary = "="
+
+  var parts = input.split(splitCharMain)
+  var obj= parts.reduce(function(acc, entity, index){
+    if(index===0) return acc;
+    const pair=entity.split(splitCharSecondary);
+    acc[pair[0]] = pair[1];
+    return acc;
+  }, {})
+  return obj
+//example: index;a=1;b=2
+}
+
+console.log()
