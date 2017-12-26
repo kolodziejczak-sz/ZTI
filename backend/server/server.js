826d2aed6f4baf895fbeece9ae9f871712611754
Tomasz Kot <kottomasz123@gmail.com>
2017-12-26 22:09:33 +0100

    matrix parsing and proper endpoint

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,10 +2,12 @@ var express = require('express'),
     app = express(),
     port = 7777;
 
+var matrixParser = require ('matrix-parser');
 var rdfController = require("../../backend/server/controllers/rdfController.js")
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
+app.use(matrixParser ())
 var rdf_s = rdfController.loadStore()
 app.listen(port);
 
@@ -48,16 +50,6 @@ app.get('/api/list/os', function (req, res) {
 app.get('/api/TEST', function (req, res) {
   console.log('TESTING')
 
-    //"@PREFIX phones: <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>" +
-    //"SELECT * {<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> ?p ?o}"//"Select * {?s ?p ?o}"
-
-  // var sparql_query ="SELECT * {<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#AcerDX900> " +
-  //   " <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#has_os> ?o}"//"Select * {?s ?p ?o}"
-
-
-  // var sparql_query ="PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>" +
-  //   "SELECT DISTINCT ?os {?p " +
-  //   " phones:has_os ?os}"//"Select * {?s ?p ?o}"
   var brand = "asus"
   var os = "droid"
   var sparql_query =" PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> " +
@@ -73,7 +65,7 @@ app.get('/api/TEST', function (req, res) {
     "?model phones:display_size ?dsize . "+
     "FILTER regex(str(?brand),\""+brand+"\", \"i\")" +
     "FILTER (xsd:integer(?price) > 80)" +
-    "FILTER regex(str(?os),\""+os+"\")"+
+    "FILTER regex(str(?os),\""+os+"\", \"i\")"+
     "FILTER (xsd:float(?dsize) > 4.0)" +
     "} "+
     " LIMIT 20"+//"Select * {?s ?p ?o}"
@@ -86,9 +78,11 @@ app.get('/api/TEST', function (req, res) {
   })
 })
 
-app.get('/api/list;matrix-uri', function (req, res) {
-  var criteria = parse(req.params.matrix-uri)
-  console.log('looking for phones fulfilling given criteria: '+criteria)
+var mpMiddleware = matrixParser ();
+app.get('/api/list*',mpMiddleware, function (req, res, next) {
+  //var criteria = parse(req.params.matrix-uri)
+  var criteria = req.matrix[1].matrix
+  console.log('looking for phones fulfilling given criteria: '+JSON.stringify(criteria))
 
   if ('ram_from' in criteria){
     var ram_from = criteria.ram_from
@@ -115,32 +109,44 @@ app.get('/api/list;matrix-uri', function (req, res) {
     var brand = criteria.brand
   }
 
-  filters = []
-  filters.append(ram_from === undefined ? "":"xsd:integer(?o) > "+ram_from)
-  filters.append(ram_to === undefined ? "":"xsd:integer(?o) < "+ram_to)
-  filters.append(os === undefined ? "":"regex(?p,'"+os+"','i')")
-  filters.append(brand === undefined ? "":"regex(?p,'"+brand+"','i')")
-  filters.append(price_from === undefined ? "":"xsd:integer(?o) > "+price_from)
-  filters.append(price_to === undefined ? "":"xsd:integer(?o) < "+price_to)
-  filters.append(display_inch_from === undefined ? "":"xsd:integer(?o) > "+display_inch_from)
-  filters.append(display_inch_to === undefined ? "":"xsd:integer(?o) < "+display_inch_to)
-  filters = filtes.filter(function (element) {
-    return element !== ""
+
+  var filters = []
+  //filters.push(ram_from === undefined ? "":"xsd:integer(?o) > "+ram_from)
+  //filters.push(ram_to === undefined ? "":"xsd:integer(?o) < "+ram_to)
+  filters.push(os === undefined ? "":"FILTER regex(str(?os),\""+os+"\", \"i\")")
+  filters.push(brand === undefined ? "":"FILTER regex(str(?brand),\""+brand+"\", \"i\")")
+  filters.push(price_from === undefined ? "":"FILTER (xsd:integer(?price) > "+price_from+")")
+  filters.push(price_to === undefined ? "":"FILTER (xsd:integer(?price) < "+price_to+")")
+  filters.push(display_inch_from === undefined ? "":"FILTER (xsd:float(?dsize) > "+display_inch_from+")")
+  filters.push(display_inch_to === undefined ? "":"FILTER (xsd:float(?dsize) <"+display_inch_to+")")
+  filters = filters.filter(function (element) {
+      return element !== ""
   })
+  var filter_string = filters.join("")
 
-  var filter_string = filters.join(" && ")
-
-  var sparql_query = "" +
-    "SELECT ?s " +
-    "WHERE "+
-    "{?s a :phone" +
-    " ?p" +
-    " ?o }" +
-    "FILTER(" +
-    "" +
-     filter_string
-      +
-    ")"
+  var sparql_query =" PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> " +
+    " PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+    " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+    " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+    " SELECT * {" +
+    "?model rdf:type phones:model ." +
+    "?model rdfs:label ?label ." +
+    "?model phones:price_eur ?price ." +
+    "?model phones:has_os ?os ." +
+    "?model phones:made_by ?brand . "+
+    "?model phones:display_size ?dsize . "+
+    filter_string+
+    //brand === undefined ? "":"FILTER regex(str(?brand),\""+brand+"\", \"i\")" +
+    // price_from === undefined ? "":"FILTER (xsd:integer(?price) > "+price_from+")" +
+    // price_to === undefined ? "":"FILTER (xsd:integer(?price) < "+price_to+")" +
+    // os === undefined ? "":"FILTER regex(str(?os),\""+os+"\")"+
+    // display_inch_from === undefined ? "":"FILTER (xsd:float(?dsize) > "+display_inch_from+")" +
+    // display_inch_to === undefined ? "":"FILTER (xsd:float(?dsize) < "+display_inch_to+")" +
+    // ram_from === undefined ? "":"FILTER (xsd:integer(?ram) > "+ram_from+")" +
+    // ram_to === undefined ? "":"FILTER (xsd:integer(?ram) < "+ram_to+")" +
+    "} "+
+    "LIMIT 20"+
+    ""
 
   console.log("query: "+sparql_query)
   rdf_s.execute(sparql_query, function (err, result) {
