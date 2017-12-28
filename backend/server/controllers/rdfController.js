1ceda4f544c951618668db3c1e55622e3fc8d9c6
Tomasz Kot <kottomasz123@gmail.com>
2017-12-28 09:31:58 +0100

    model+brand endpoint

--- backend/server/controllers/rdfController.js
+++ backend/server/controllers/rdfController.js
@@ -1,16 +1,16 @@
 module.exports = {
   queryRdfStore: function queryRdfStore(query, callback) {
     "use strict";
-    var fs = require('fs');
-    var rdfstore = require('rdfstore')
-    var path = require('path');
+    const fs = require('fs');
+    const rdfstore = require('rdfstore');
+    const path = require('path');
     rdfstore.create(function (err, store) {
 
-      console.log(__dirname)
-      var filePath = path.join(__dirname, '..', '..', '..', 'ontology', 'phones-turtle.owl');
-      var inputString = fs.readFileSync(filePath).toString()
+      console.log(__dirname);
+      const filePath = path.join(__dirname, '..', '..', '..', 'ontology', 'phones-turtle.owl');
+      const inputString = fs.readFileSync(filePath).toString();
 
-      const type = "text/turtle"//"application/ld+json"
+      const type = "text/turtle";//"application/ld+json"
       store.load(type, inputString, function (err, results) {
         console.log("elements loaded into store: " + results);
 
@@ -28,29 +28,29 @@ module.exports = {
 
   loadStore: function loadStore() {
 
-    var fs = require('fs');
-    var rdfstore = require('rdfstore')
-    var path = require('path');
+    const fs = require('fs');
+    const rdfstore = require('rdfstore');
+    const path = require('path');
 
     const filePath = path.join(__dirname, '..', '..', '..', 'ontology', 'phones-turtle.owl');
-    var inputString = fs.readFileSync(filePath).toString()
-    const type = "text/turtle"//"application/ld+json"
-    var full_store
-    var s = rdfstore.create(function (err, store) {
+    var inputString = fs.readFileSync(filePath).toString();
+    const type = "text/turtle";//"application/ld+json"
+    var full_store;
+    rdfstore.create(function (err, store) {
 
-      console.log(__dirname)
+      console.log(__dirname);
 
       store.load(type, inputString, function (err, results) {
         console.log("elements loaded into store: " + results);
         full_store = store
       })
 
-    })
+    });
     while (full_store === undefined) {
       require('deasync').runLoopOnce()
     }
     return full_store
   }
 
-}
+};
 
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,58 +1,134 @@
-var express = require('express'),
-    app = express(),
-    port = 7777;
+const express = require('express'),
+  app = express(),
+  port = 7777;
 
-var matrixParser = require ('matrix-parser');
-var rdfController = require("../../backend/server/controllers/rdfController.js")
+const matrixParser = require('matrix-parser');
+const rdfController = require("../../backend/server/controllers/rdfController.js");
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.json())
-app.use(matrixParser ())
-var rdf_s = rdfController.loadStore()
+app.use(bodyParser.json());
+app.use(matrixParser ());
+let rdf_s = rdfController.loadStore();
 app.listen(port);
 
 app.put('/', function (req, res) {
-  console.log(req.body)
-  var params = req.query
-  var sparql_query = req.body.query
+  console.log(req.body);
+  var params = req.query;
+  var sparql_query = req.body.query;
 
   rdf_s.execute(sparql_query, function (err, result) {
-    console.log(result)
+    console.log(result);
+    res.send(result)
+  })
+});
+const mpMiddleware = matrixParser();
+
+//example: AmoiM360
+app.get('/api/detail*', mpMiddleware, function (req, res) {
+  var criteria = req.matrix[1].matrix;
+  var brand = criteria.brand;
+  var model = criteria.model;
+  console.log('looking for data about: '+model+" made by "+brand);
+
+  // var sparql_query = "SELECT * " +
+  //   "{<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#"+req.params.model+"> ?p ?o }"
+
+  var sparql_query = ""+
+    " PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> " +
+    " PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+    " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+    " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+    " SELECT * {" +
+    "?model rdf:type phones:model ." +
+    "?model phones:made_by ?brand . "+
+    "?model ?p ?s ."+
+    "FILTER regex(str(?brand),\""+brand+"\", \"i\")" +
+    "FILTER regex(str(?model),\""+model+"\", \"i\")" +
+    "} "+
+    " LIMIT 20"+//"Select * {?s ?p ?o}"
+    "";
+
+  console.log("query: "+sparql_query);
+  rdf_s.execute(sparql_query, function (err, result) {
+    console.log(result);
     res.send(result)
   })
 });
 
-//example: AmoiM360
-app.get('/api/detail/:model', function (req, res) {
-  console.log('looking for data about: '+req.params.model)
-  var sparql_query = "SELECT * {<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#"+req.params.model+"> ?p ?o }"
-  console.log("query: "+sparql_query)
-  rdf_s.execute(sparql_query, function (err, result) {
-    console.log(result)
-    res.send(result)
-  })
-})
-
 app.get('/api/list/os', function (req, res) {
-  console.log('listing operating systems')
-  var sparql_query = " PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>" +
-    "SELECT DISTINCT ?os {?p " +
-    " phones:has_os ?os}"//"Select * {?s ?p ?o}"
+  console.log('listing operating systems');
+  var sparql_query = "" +
+    " PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> " +
+    " PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+    " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+    " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+    " PREFIX owl: <http://www.w3.org/2002/07/owl#>"+
+    "SELECT DISTINCT ?family {" +
+     "?p phones:has_os ?os ."+
+     "?os rdf:type ?family ."+
+     //"?family rdfs:subClassOf phones:OS ."+
+    // "?family rdf:type phones:OS ."+
+    // "filter not exists { ?os rdf:type owl:NamedIndividual }"+
+    // "?entity rdf:type ?type ." +
+    //"?type rdfs:subClassOf phones:OS ."+
+     //"?subclass rdfs:subClassOf ?c ." +
+    //"?entity rdf:type ?subclass ."+
+    // "MINUS {\n" +
+    // "      ?family ?p \"NamedIndividual\" ." +
+    // "   }"+
+    "}"
+    // "SELECT DISTINCT ?os {?p " +
+    // " phones:has_os ?os}";//"Select * {?s ?p ?o}"
 
-  console.log("query: "+sparql_query)
+  console.log("query: "+sparql_query);
   rdf_s.execute(sparql_query, function (err, result) {
-    console.log(result)
-    res.send(result)
+
+    if(err) {
+      res.status(500).send({
+        message:'Some error occured while fetching os list.',
+        description:err
+      });
+    }
+    else {
+
+      console.log(result);
+      res.send(result)
+    }
   })
-})
+});
+
+app.get('/api/list/brands', function (req, res) {
+  console.log('listing operating systems');
+  var sparql_query = " PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>" +
+    "SELECT DISTINCT ?brand {?p " +
+    " phones:made_by ?brand}";//"Select * {?s ?p ?o}"
+
+  console.log("query: "+sparql_query);
+  rdf_s.execute(sparql_query, function (err, result) {
+
+    if(err) {
+      res.status(500).send({
+        message:'Some error occured while fetching brand list.',
+        description:err
+      });
+    }
+    else {
+
+      console.log(result);
+      res.send(result)
+    }
+  })
+});
+
 
 //AcerDX900
 app.get('/api/TEST', function (req, res) {
-  console.log('TESTING')
+  console.log('TESTING');
 
-  var brand = "asus"
-  var os = "droid"
-  var sparql_query =" PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> " +
+  var brand = "asus";
+  var os = "droid";
+  var sparql_query ="" +
+    " PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> " +
     " PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
     " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
     " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
@@ -69,20 +145,20 @@ app.get('/api/TEST', function (req, res) {
     "FILTER (xsd:float(?dsize) > 4.0)" +
     "} "+
     " LIMIT 20"+//"Select * {?s ?p ?o}"
-    ""
-  console.log("query: "+sparql_query)
+    "";
+  console.log("query: "+sparql_query);
   rdf_s.execute(sparql_query, function (err, result) {
 
-    console.log(result)
+    console.log(result);
     res.send(result)
   })
-})
+});
 
-var mpMiddleware = matrixParser ();
+
 app.get('/api/list*',mpMiddleware, function (req, res, next) {
   //var criteria = parse(req.params.matrix-uri)
-  var criteria = req.matrix[1].matrix
-  console.log('looking for phones fulfilling given criteria: '+JSON.stringify(criteria))
+  var criteria = req.matrix[1].matrix;
+  console.log('looking for phones fulfilling given criteria: '+JSON.stringify(criteria));
 
   if ('ram_from' in criteria){
     var ram_from = criteria.ram_from
@@ -110,19 +186,19 @@ app.get('/api/list*',mpMiddleware, function (req, res, next) {
   }
 
 
-  var filters = []
+  var filters = [];
   //filters.push(ram_from === undefined ? "":"xsd:integer(?o) > "+ram_from)
   //filters.push(ram_to === undefined ? "":"xsd:integer(?o) < "+ram_to)
-  filters.push(os === undefined ? "":"FILTER regex(str(?os),\""+os+"\", \"i\")")
-  filters.push(brand === undefined ? "":"FILTER regex(str(?brand),\""+brand+"\", \"i\")")
-  filters.push(price_from === undefined ? "":"FILTER (xsd:integer(?price) > "+price_from+")")
-  filters.push(price_to === undefined ? "":"FILTER (xsd:integer(?price) < "+price_to+")")
-  filters.push(display_inch_from === undefined ? "":"FILTER (xsd:float(?dsize) > "+display_inch_from+")")
-  filters.push(display_inch_to === undefined ? "":"FILTER (xsd:float(?dsize) <"+display_inch_to+")")
+  filters.push(os === undefined ? "":"FILTER regex(str(?os),\""+os+"\", \"i\")");
+  filters.push(brand === undefined ? "":"FILTER regex(str(?brand),\""+brand+"\", \"i\")");
+  filters.push(price_from === undefined ? "":"FILTER (xsd:integer(?price) > "+price_from+")");
+  filters.push(price_to === undefined ? "":"FILTER (xsd:integer(?price) < "+price_to+")");
+  filters.push(display_inch_from === undefined ? "":"FILTER (xsd:float(?dsize) > "+display_inch_from+")");
+  filters.push(display_inch_to === undefined ? "":"FILTER (xsd:float(?dsize) <"+display_inch_to+")");
   filters = filters.filter(function (element) {
       return element !== ""
-  })
-  var filter_string = filters.join("")
+  });
+  var filter_string = filters.join("");
 
   var sparql_query =" PREFIX phones:<http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#> " +
     " PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
@@ -146,31 +222,25 @@ app.get('/api/list*',mpMiddleware, function (req, res, next) {
     // ram_to === undefined ? "":"FILTER (xsd:integer(?ram) < "+ram_to+")" +
     "} "+
     "LIMIT 20"+
-    ""
+    "";
 
-  console.log("query: "+sparql_query)
+  console.log("query: "+sparql_query);
   rdf_s.execute(sparql_query, function (err, result) {
-    console.log(result)
-    res.send(result)
+
+    if(err) {
+      res.status(500).send({
+        message:'Some error occured while fetchnig query results',
+        description:err
+      });
+    }
+    else {
+
+      console.log(result);
+      res.send(result)
+    }
   })
-})
+});
 
 
 console.log('REST API started on: http://localhost:' + port);
-
-function parse(input) {
-  const splitCharMain = ";"
-  const splitCharSecondary = "="
-
-  var parts = input.split(splitCharMain)
-  var obj= parts.reduce(function(acc, entity, index){
-    if(index===0) return acc;
-    const pair=entity.split(splitCharSecondary);
-    acc[pair[0]] = pair[1];
-    return acc;
-  }, {})
-  return obj
-//example: index;a=1;b=2
-}
-
-console.log()
+console.log();
