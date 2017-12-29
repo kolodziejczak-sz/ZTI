9025fab3d48c1c8923febf8c8403103623ea9730
Tomasz Kot <kottomasz123@gmail.com>
2017-12-29 23:33:10 +0100

    feature parity with old backend

--- jena-backend/src/main/java/zti/Backend.java
+++ jena-backend/src/main/java/zti/Backend.java
@@ -1,25 +1,16 @@
 package zti;
 
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
-import lombok.SneakyThrows;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Set;
+import java.util.Map;
 
 import javax.ws.rs.QueryParam;
 
 @RestController
 public class Backend {
-
-    private final String ONTOLOGY_PREFIX = "http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#";
 
     private QueryExecutor executor;
 
@@ -28,47 +19,88 @@ public class Backend {
         this.executor = queryExecutor;
     }
 
-    @RequestMapping("/query")
-    public String processQuery(@QueryParam("query") String query) {
+    @RequestMapping("/api/os")
+    public String getOperatingSystems() {
 
-        query = query.replaceAll("\n","");
+        String osQuery = "PREFIX phones: <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>\n"+
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+        "SELECT ?x WHERE { ?x rdfs:subClassOf phones:OS . }\n";
 
-        QueryExecution qexec = QueryExecutionFactory.create( query, executor.model );
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try {
-            ResultSet results = qexec.execSelect();
-            //ResultSetFormatter.out( results, executor.model );
-            ResultSetFormatter.outputAsJSON(outputStream, results);
-        }
-        finally {
-            qexec.close();
-        }
-
-        String json = new String(outputStream.toByteArray());
-
-        return cleanJson(json);
+        return executor.execute(osQuery);
     }
 
-    @SneakyThrows
-    private String cleanJson(String jsonString) {
+    @RequestMapping("/api/brands")
+    public String getBrands() {
 
+        String brandsQuery = "PREFIX phones: <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>\n" +
+                "  SELECT DISTINCT ?x {?p phones:made_by ?x}\n";
 
-        JsonObject json = new JsonObject(jsonString);
-
-        JsonArray bindings = json.getJsonObject("results").getJsonArray("bindings");
-
-        JsonArray updatedBinding = new JsonArray();
-        bindings.stream().map(element -> (JsonObject) element)
-                .map(element -> {
-                    Set<String> keys = element.getMap().keySet();
-                    for (String key: keys) {
-                        JsonObject innerJson = element.getJsonObject(key);
-                        String updatedString = innerJson
-                                .getString("value").replaceAll(ONTOLOGY_PREFIX, "");
-                        innerJson.put("value", updatedString);
-                    }
-                    return element;
-                }).forEach(updatedBinding::add);
-        return updatedBinding.toString();
+        return executor.execute(brandsQuery);
     }
+
+    //http://localhost:8080/api/detail/?model=A21
+    @RequestMapping("/api/detail/")
+    public String getDetails(@QueryParam("model") String model) {
+
+        String modelQuery =
+        "PREFIX phones: <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>\n"+
+        "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+        "SELECT * {\n"+
+                "?model rdf:type phones:model .\n"+
+                "?model rdfs:label ?label .\n"+
+                "?model phones:image ?img .\n"+
+                "?model phones:made_by ?brand .\n"+
+                "?model ?p ?s .\n"+
+        "FILTER regex(?label,\""+model+"\")\n"+
+    "} LIMIT 1" ;
+        return executor.execute(modelQuery);
+    }
+
+    //http://localhost:8080/api/search/;price_to=200;os=android;display_inch_from=1
+    @RequestMapping("/api/search/{data}")
+    //public String search(@MatrixVariable(name = "price_to", required = false) String price) {
+    public String search(@MatrixVariable Map<String, String> matrixVars) {
+
+
+        String searchQuery = "" +
+                "PREFIX phones: <http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#>\n" +
+                "    PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "    " +
+                "    SELECT ?model ?img ?label ?os ?brand ?price ?ram ?dsize {\n" +
+                "      ?model rdf:type phones:model .\n" +
+                "      ?model phones:image ?img .\n" +
+                "      ?model rdfs:label ?label .\n" +
+                "      ?model phones:has_os ?os .\n" +
+                "      ?model phones:made_by ?brand .\n" +
+                "      ?model phones:price_eur ?price .\n" +
+                "      ?model phones:display_size ?dsize .\n" +
+                "      ?model phones:has_ram ?ram .\n" +
+                getFilters(matrixVars)+
+                "    } LIMIT 20";
+
+        return executor.execute(searchQuery);
+    }
+
+
+    private String getFilters(Map<String,String> map){
+        String filters = "";
+
+        filters += map.containsKey("query") ? "FILTER (CONTAINS(?model,\" "+map.get("query")+"\"))\n" : "";
+        filters += map.containsKey("os") ? "FILTER (CONTAINS(?os, \""+map.get("os")+"\"))\n" : "";
+        filters += map.containsKey("brand") ? "FILTER (CONTAINS(?brand, \""+map.get("brand")+"\"))\n" : "";
+        filters += map.containsKey("price_from") ? "FILTER (xsd:float(?price) > "+map.get("price_from")+"))\n" : "";
+        filters += map.containsKey("price_to") ? "FILTER (xsd:float(?price) < "+map.get("price_to")+"))\n" : "";
+        filters += map.containsKey("ram_from") ? "FILTER (xsd:float(?ram) > "+map.get("ram_from")+"))\n" : "";
+        filters += map.containsKey("ram_to") ? "FILTER (xsd:float(?ram) < "+map.get("ram_to")+"))\n" : "";
+        filters += map.containsKey("display_inch_from") ? "FILTER (xsd:float(?dsize) > "+map.get("display_inch_from")+"))\n" : "";
+        filters += map.containsKey("display_inch_to") ? "FILTER (xsd:float(?dsize) < "+map.get("display_inch_to")+"))\n" : "";
+
+        return filters;
+    }
+
+
+
 }
--- jena-backend/src/main/java/zti/QueryExecutor.java
+++ jena-backend/src/main/java/zti/QueryExecutor.java
@@ -1,16 +1,68 @@
 package zti;
 
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.apache.jena.ontology.OntModel;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.io.ByteArrayOutputStream;
+import java.util.Set;
+
 
 @AllArgsConstructor
 public class QueryExecutor {
+    private final String ONTOLOGY_PREFIX = "http://www.semanticweb.org/ZTI/ontologies/2017/10/phones#";
 
     OntModel model;
+
+    String execute(String query){
+        System.out.println(query);
+        query = query.replaceAll("\n","");
+
+        QueryExecution qexec = QueryExecutionFactory.create( query, model );
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            ResultSet results = qexec.execSelect();
+            //ResultSetFormatter.out( results, executor.model );
+            ResultSetFormatter.outputAsJSON(outputStream, results);
+        }
+        finally {
+            qexec.close();
+        }
+
+        String json = new String(outputStream.toByteArray());
+
+        return cleanJson(json);
+    }
+
+    @SneakyThrows
+    private String cleanJson(String jsonString) {
+
+
+        JsonObject json = new JsonObject(jsonString);
+
+        JsonArray bindings = json.getJsonObject("results").getJsonArray("bindings");
+
+        JsonArray updatedBinding = new JsonArray();
+        bindings.stream().map(element -> (JsonObject) element)
+                .map(element -> {
+                    Set<String> keys = element.getMap().keySet();
+                    for (String key: keys) {
+                        JsonObject innerJson = element.getJsonObject(key);
+                        String updatedString = innerJson
+                                .getString("value").replaceAll(ONTOLOGY_PREFIX, "");
+                        innerJson.put("value", updatedString);
+                    }
+                    return element;
+                }).forEach(updatedBinding::add);
+        String clean = updatedBinding.toString();
+        System.out.println(clean);
+        return clean;
+    }
 
 }
