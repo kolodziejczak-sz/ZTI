018ba6e103b89626d6e27f645f1b5284de9a97e4
Tomasz Kot <kottomasz123@gmail.com>
2018-01-03 09:42:34 +0100

    cors

--- jena-backend/src/main/java/zti/Backend.java
+++ jena-backend/src/main/java/zti/Backend.java
@@ -1,9 +1,10 @@
 package zti;
 
-import static zti.FormattingStrategy.NESTED;
 import static zti.FormattingStrategy.FLAT;
+import static zti.FormattingStrategy.NESTED;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Map;
 import javax.ws.rs.QueryParam;
 
 @RestController
+@CrossOrigin
 public class Backend {
 
     private QueryExecutor executor;
